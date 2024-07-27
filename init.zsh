a2a297788df298eeeaefd51e6338b37b76ce5862
Kahlil Whitfield <darkristy2002@gmail.com>
2024-07-27 03:05:35 -0400

    chore(*): create initial setup file *work in progress*

---
+++ init.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+universal_packages="zsh neovim tmux volta hub stow antidote starship peco rustup"
+universal_programs="visual-studio-code alacrity"
+macos_programs=""
+rust_crates="eza"
