36c697aa2c9fafe116689524a58b592a57050731
Kahlil Whitfield <darkristy2002@gmail.com>
2024-07-27 14:23:12 -0400

    chore(init.zsh): remove old setup file

--- init.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-universal_packages="zsh neovim tmux volta hub stow antidote starship peco rustup"
-universal_programs="visual-studio-code alacrity"
-macos_programs=""
-rust_crates="eza"
